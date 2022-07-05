35ed4c20d66085f571deca2c0a716e2cdefbd84b
Ben Manley <benjamintmanley@gmail.com>
2022-07-04 22:07:39 -0500

    add reading create

--- controllers/readings.js
+++ controllers/readings.js
@@ -1,6 +1,14 @@
 import { Reading } from "../models/reading.js"
 
+function create(req, res) {
+  if (req.body.accessKey === process.env.ACCESS_KEY) {
+    Reading.create(req.body)
+    .then(()=> {
+      res.status(200).json('Successful')
+    })
+  }
+}
 
 export {
-  
+  create
 }
